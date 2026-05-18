cba80375fa3fcebdf0d3e6b412f2990626993a8c
KimByungKook <qw010102@gmail.com>
2026-05-18 17:34:34 +0900

    Feat: 공통 Modal 구현

--- src/main/webapp/resources/common/common.js
+++ src/main/webapp/resources/common/common.js
@@ -54,3 +54,43 @@ function escapeHtml(str) {
     .replace(/"/g, '&quot;')
     .replace(/'/g, '&#039;');
 }
+
+var modalCallback = null;
+
+function showAlert(message, callback) {
+    $('#modalBody').html(message.replace(/\n/g, '<br>'));
+    $('#modalFooter').html(
+        '<button type="button" class="btn btn-primary btn-lg" ' +
+        'data-bs-dismiss="modal" onclick="modalClose()">확인</button>'
+    );
+    modalCallback = callback || null;
+    $('#commonModal').modal('show');
+}
+
+function showConfirm(message, confirmCallback, cancelCallback) {
+    $('#modalBody').html(message.replace(/\n/g, '<br>'));
+    $('#modalFooter').html(
+        '<button type="button" class="btn btn-primary btn-lg me-3" ' +
+        'onclick="modalConfirm()">확인</button>' +
+        '<button type="button" class="btn btn-secondary btn-lg" ' +
+        'onclick="modalCancel()">취소</button>'
+    );
+    modalCallback = confirmCallback || null;
+    window.modalCancelCallback = cancelCallback || null;
+    $('#commonModal').modal('show');
+}
+
+function modalConfirm() {
+    $('#commonModal').modal('hide');
+    if(typeof modalCallback === 'function') modalCallback();
+}
+
+function modalCancel() {
+    $('#commonModal').modal('hide');
+    if(typeof window.modalCancelCallback === 'function') window.modalCancelCallback();
+}
+
+function modalClose() {
+    $('#commonModal').modal('hide');
+    if(typeof modalCallback === 'function') modalCallback();
+}
