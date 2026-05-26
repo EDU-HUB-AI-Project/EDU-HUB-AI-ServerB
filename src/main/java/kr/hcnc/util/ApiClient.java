0bac71c69b65c18693bc7893edbb778831cf7414
KimByungKook <qw010102@gmail.com>
2026-05-27 01:00:19 +0900

    Feat: Https 프로토콜을 사용한 보안처리

--- src/main/java/kr/hcnc/util/ApiClient.java
+++ src/main/java/kr/hcnc/util/ApiClient.java
@@ -2,23 +2,44 @@ package kr.hcnc.util;
 
 
 
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import javax.net.ssl.SSLContext;
 
 @Component("apiClient")
 public class ApiClient {
 	
-	private RestTemplate restTemplate = new RestTemplate();
+	private RestTemplate restTemplate;
 	
 	@Value("${Globals.ApiClient}")
 	private String baseUrl;
 	
 	@Value("${Globals.ApiSecretKey}")
 	private String apiSecretKey;
+	
+	
+	@PostConstruct
+	public void init() throws Exception {
+		SSLContext sslContext = SSLContextBuilder.create()
+			    .loadTrustMaterial((chain, authType) -> true)
+			    .build();
+		
+		CloseableHttpClient httpClient = HttpClients.custom()
+										.setSSLContext(sslContext)
+										.build();
+		
+		this.restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
+	}
 	
 	private HttpHeaders createHeaders() {
 		HttpHeaders headers = new HttpHeaders();
@@ -27,12 +48,12 @@ public class ApiClient {
 	}
 
 	public <T> T get(String url, Class<T> responseType) {
-		HttpEntity<?> entity = new HttpEntity<>(createHeaders());
-		return restTemplate.exchange(baseUrl + url, HttpMethod.GET, entity, responseType).getBody();
-	}
-	
-	public <T> T post(String url, Object requestBody, Class<T> responseType) {
-		HttpEntity<?> entity = new HttpEntity<>(requestBody, createHeaders());
-		return restTemplate.postForObject(baseUrl + url, entity, responseType);
-	}
+        HttpEntity<?> entity = new HttpEntity<>(createHeaders());
+        return restTemplate.exchange(baseUrl + url, HttpMethod.GET, entity, responseType).getBody();
+    }
+
+    public <T> T post(String url, Object requestBody, Class<T> responseType) {
+        HttpEntity<?> entity = new HttpEntity<>(requestBody, createHeaders());
+        return restTemplate.postForObject(baseUrl + url, entity, responseType);
+    }
 }
