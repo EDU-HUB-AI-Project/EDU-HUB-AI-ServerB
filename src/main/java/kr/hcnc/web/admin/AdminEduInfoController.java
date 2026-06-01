17447cd7045a246c6332f09e741f1409e77e4cc8
devqw010102 <qw010102@gmail.com>
2026-06-01 17:15:15 +0900

    Feat: 관리자용 교육과정, 과목 API 엔드포인트 구현 완료

---
+++ src/main/java/kr/hcnc/web/admin/AdminEduInfoController.java
@@ -0,0 +1,65 @@
+package kr.hcnc.web.admin;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.hcnc.service.admin.AdminEduInfoService;
+import kr.hcnc.vo.EduInfoVO;
+
+@RestController
+@RequestMapping("/admin/eduInfo")
+public class AdminEduInfoController {
+
+	@Resource(name = "adminEduInfoService")
+	private AdminEduInfoService adminEduInfoService;
+	
+	private static final Logger log = LoggerFactory.getLogger(AdminEduInfoController.class);
+	
+	@GetMapping
+	public ResponseEntity<List<EduInfoVO>> getEduInfo() {
+		log.info("Called :: GET /admin/eduInfo");
+		return ResponseEntity.ok(adminEduInfoService.selectEduInfo());
+	}
+	
+	@GetMapping("/{eduInfoId}")
+	public ResponseEntity<EduInfoVO> getEduInfoById(@PathVariable String eduInfoId) {
+		log.info("Called :: GET /admin/eduInfo/{}", eduInfoId);
+		return ResponseEntity.ok(adminEduInfoService.selectEduInfoById(eduInfoId));
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> insertEduInfo(@RequestBody EduInfoVO eduInfoVO) {
+		log.info("Called :: POST /admin/eduInfo");
+		Map<String, Object> response = adminEduInfoService.insertEduInfo(eduInfoVO);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);  
+	}
+	
+	@PutMapping("/{eduInfoId}")
+	public ResponseEntity<?> updateEduInfo(@PathVariable String eduInfoId, @RequestBody EduInfoVO eduInfoVO) {
+		log.info("Called :: PUT /admin/eduInfo/{}", eduInfoId);
+		Map<String, Object> response = adminEduInfoService.updateEduInfo(eduInfoId, eduInfoVO);
+		return ResponseEntity.ok(response);
+	}
+	
+	@DeleteMapping("/{eduInfoId}")
+	public ResponseEntity<?> deleteEduInfo(@PathVariable String eduInfoId) {
+		log.info("Called :: DELETE /admin/eduInfo/{}", eduInfoId);
+		adminEduInfoService.deleteEduInfo(eduInfoId);
+		return ResponseEntity.ok().build();
+	}
+}
