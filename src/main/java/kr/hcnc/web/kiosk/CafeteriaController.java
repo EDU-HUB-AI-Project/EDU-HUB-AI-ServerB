f3efc260225a04295c89bfdf759cc0a7f75b6d62
Park sihyeno <everyhigh6554@gmail.com>
2026-06-05 18:05:07 +0900

    fix: CafeteriaController RequestMapping 누락 수정

--- src/main/java/kr/hcnc/web/kiosk/CafeteriaController.java
+++ src/main/java/kr/hcnc/web/kiosk/CafeteriaController.java
@@ -10,11 +10,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.hcnc.service.kiosk.CafeteriaService;
 
 @Controller
+@RequestMapping("/facility")
 public class CafeteriaController {
 	
 	@Resource(name = "cafeteriaService")
